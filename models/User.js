84d0405316e8f888438bb186d31aa6c755de9ad7
Kauã Lima <lima.kaua2006@gmail.com>
2022-04-22 12:31:38 -0300

    User model created

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const generateUserId = require('../src/generateUserId.js');
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+        default: generateUserId(),
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    contactList: {
+        type: Object,
+        required: true,
+        default: [],
+    }
+});
