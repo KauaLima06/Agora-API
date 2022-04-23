eaf1b6d0154c43a162b495b65757f9f07b690dc7
Kauã Lima <lima.kaua2006@gmail.com>
2022-04-23 13:09:25 -0300

    1/4 completed

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const apiRouters = require('./routers/apiRouters.js');
+const userRouters = require('./routers/userRouters.js');
 
 const app = express();
 app.use(express.json());
-app.use('/user', apiRouters);
+app.use('/user', userRouters);
 
-//mongodb+srv://KauaLima06:<password>@api-db.0xuhv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 const port = process.env.PORT || 2929;
 //Connecting data base
-mongoose.connect(`mongodb+srv://${db_user}:${db_pass}@api-db.0xuhv.mongodb.net/agora-api?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${db_user}:${db_pass}
+@api-db.0xuhv.mongodb.net/agora-api?retryWrites=true&w=majority`)
     .then(() => {
         console.log('Mongo was connected');
         app.listen(port, () => {
--- routers/apiRouters.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'Agora API'});
-});
-
-module.exports = router;
