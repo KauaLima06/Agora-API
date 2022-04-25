05975cc7727632e0b4a01371b04da484f1344b70
Kauã Lima <lima.kaua2006@gmail.com>
2022-04-25 09:21:48 -0300

    Rename model

---
+++ models/Chat.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const generateId = require('../src/generateId.js');
+
+const chatSchema = new Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    members: {
+        type: String,
+        required: true,
+        default: '',
+    },
+    chatId: {
+        type: String,
+        required: true,
+        default: generateId,
+    },
+    messages: {
+        type: String,
+        required: false,
+        default: '',
+    }
+});
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+module.exports = Chat;
